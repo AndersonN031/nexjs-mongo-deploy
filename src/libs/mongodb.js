fa796926b590befd6bc29479cfe3420600c5e928
AndersonN031 <devAndersonnunes078@gmail.com>
2024-04-16 08:28:45 -0300

    agr vai

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
 export default async function connectDB() {
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.MONGODB_URL, {
+        family: 4
+    })
+
+        .then(() => {
+            console.log("FINE");
+        })
+        .catch(() => {
+            console.log("BAD");
+        })
 }
