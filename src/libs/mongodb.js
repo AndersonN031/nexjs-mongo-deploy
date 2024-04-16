11abe41a786884d293dfdc6c52f2e70df57e5c42
AndersonN031 <devAndersonnunes078@gmail.com>
2024-04-16 06:58:00 -0300

    timeout

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
 export default async function connectDB() {
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.MONGODB_URL, {
+        serverSelectionTimeoutMS: 30000, // 30s
+        socketTimeoutMS: 45000, // 45s
+    })
 }
