016e2d828bc5e200a387829e6dde1e153b3ba0c2
AndersonN031 <devAndersonnunes078@gmail.com>
2024-04-16 07:04:57 -0300

    test

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
-export default async function connectDB() {
-    await mongoose.connect(process.env.MONGODB_URL, {
+export default function connectDB() {
+    return mongoose.connect(process.env.MONGODB_URL, {
         serverSelectionTimeoutMS: 30000, // 30s
         socketTimeoutMS: 45000, // 45s
-    })
+    }).then(() => {
+        console.log("MongoDB connected successfully");
+    }).catch(err => {
+        console.error("MongoDB connection error:", err);
+    });
 }
